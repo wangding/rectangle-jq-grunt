16066ea1f253e2c7192ac84e4972c441f335148b
wangding <408542507@qq.com>
2019-03-05 17:47:44 +0800

    v0.2 static code analysis and travis ci

---
+++ Gruntfile.js
@@ -0,0 +1,29 @@
+/* global module: true */
+module.exports = function (grunt) {
+  grunt.initConfig({
+    eslint: {
+      options: {
+        configFile: '.eslintrc.json'
+      },
+      target: ['*.js']
+    },
+    csslint: {
+      options: {
+        csslintrc: '.csslintrc'
+      },
+      src: '*.css'
+    },
+    htmlhint: {
+      options: {
+        htmlhintrc: '.htmlhintrc'
+      },
+      src: '*.html'
+    }
+  });
+
+  grunt.loadNpmTasks('grunt-contrib-csslint');
+  grunt.loadNpmTasks('grunt-htmlhint');
+  grunt.loadNpmTasks('grunt-eslint');
+
+  grunt.registerTask('lint', ['htmlhint', 'csslint', 'eslint']);
+};
