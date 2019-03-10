4cffc3f00e5e1694f928a4f45771a744424ec673
wangding <408542507@qq.com>
2019-03-10 10:10:00 +0800

    v0.4 minify code

--- Gruntfile.js
+++ Gruntfile.js
@@ -27,6 +27,27 @@ module.exports = function (grunt) {
         run: true,
         reporter: 'Spec'
       }
+    },
+    htmlmin: {
+      options: {
+        collapseWhitespace: true,
+        preserveLineBreaks: false
+      },
+      files: {
+        src: './index.html',
+        dest: 'dist/index.html'
+      }
+    },
+    cssmin: {
+      'dist/rectangle.css': 'rectangle.css'
+    },
+    uglify: {
+      release:{
+        files: {
+          'dist/rectangle.js': 'rectangle.js',
+          'dist/calc.js': 'calc.js'
+        }
+      }
     }
   });
 
@@ -34,7 +55,11 @@ module.exports = function (grunt) {
   grunt.loadNpmTasks('grunt-htmlhint');
   grunt.loadNpmTasks('grunt-eslint');
   grunt.loadNpmTasks('grunt-mocha');
+  grunt.loadNpmTasks('grunt-contrib-htmlmin');
+  grunt.loadNpmTasks('grunt-contrib-cssmin');
+  grunt.loadNpmTasks('grunt-contrib-uglify');
 
   grunt.registerTask('lint', ['htmlhint', 'csslint', 'eslint']);
   grunt.registerTask('unitTest', ['mocha']);
+  grunt.registerTask('minify', ['htmlmin', 'cssmin', 'uglify']);
 };
