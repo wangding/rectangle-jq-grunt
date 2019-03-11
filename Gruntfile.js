ea95d134edb47645a59481eaf00a77e07bc24454
wangding <408542507@qq.com>
2019-03-11 16:55:36 +0800

    v0.5 improved ui

--- Gruntfile.js
+++ Gruntfile.js
@@ -34,7 +34,7 @@ module.exports = function (grunt) {
         preserveLineBreaks: false
       },
       files: {
-        src: './index.html',
+        src: 'dist/index.html',
         dest: 'dist/index.html'
       }
     },
@@ -44,9 +44,33 @@ module.exports = function (grunt) {
     uglify: {
       release:{
         files: {
-          'dist/rectangle.js': 'rectangle.js',
-          'dist/calc.js': 'calc.js'
+          'dist/bundle.min.js': 'dist/bundle.js',
         }
+      }
+    },
+    useminPrepare: {
+      html: 'index.html',
+      options: {
+        dest: 'dist'
+      }
+    },
+    usemin: {
+      html: ['dist/index.html']
+    },
+    concat: {
+      options: {
+        separator: ';'
+      },
+      js: {
+        src: ['rectangle.js', 'calc.js'],
+        dest: 'dist/bundle.js'
+      }
+    },
+    clean: ['dist/bundle.js', '.tmp'],
+    copy: {
+      html: {
+        src: './index.html',
+        dest: './dist/index.html'
       }
     }
   });
@@ -58,8 +82,12 @@ module.exports = function (grunt) {
   grunt.loadNpmTasks('grunt-contrib-htmlmin');
   grunt.loadNpmTasks('grunt-contrib-cssmin');
   grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-contrib-concat');
+  grunt.loadNpmTasks('grunt-contrib-copy');
+  grunt.loadNpmTasks('grunt-usemin');
+  grunt.loadNpmTasks('grunt-contrib-clean');
 
   grunt.registerTask('lint', ['htmlhint', 'csslint', 'eslint']);
   grunt.registerTask('unitTest', ['mocha']);
-  grunt.registerTask('minify', ['htmlmin', 'cssmin', 'uglify']);
+  grunt.registerTask('release', ['copy', 'useminPrepare', 'concat', 'uglify', 'usemin', 'cssmin', 'htmlmin', 'clean']);
 };
