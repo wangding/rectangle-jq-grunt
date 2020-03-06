ef2494263e470a46ec04370752b8c104fde42b93
looying <61852956+looying@users.noreply.github.com>
2020-03-06 09:28:49 +0800

    fix: 修改了浮点舍入误差问题

--- rectangle.js
+++ rectangle.js
@@ -1,3 +1,14 @@
+    /**
+    * 保留小数点后第 n 位
+    *
+    * @param x 做近似处理的数
+    * @param n 小数点后第 n 位
+    * @returns 近似处理后的数
+    */
+    function roundFractional(x, n) {
+      return Math.round(x * Math.pow(10, n)) / Math.pow(10, n);
+    }
+
 $(function() {
   var $width = $('#width'),
       $height = $('#height'),
@@ -12,7 +23,9 @@ $(function() {
     var p = 2 * (w + h),
         a = w * h;
 
-    $perimeter.val(p);
-    $area.val(a);
+    $perimeter.val(roundFractional(p, 3));
+    $area.val(roundFractional(a, 3));
   });
+
 });
+
